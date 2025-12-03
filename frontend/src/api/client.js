fad8c5f850d6c60aaa692117c0fd2617d904a8a8
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-12-03 18:35:18 +0530

    fixing the url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://wavenetproject.vercel.app/api',
-  withCredentials: true,
-})
+  baseURL: 'https://wavenet-project-nao9.vercel.app/api',
+  withCredentials: true, 
+});
+
 
 export default api
